e7ff643320860f98e8e7ac38da711d7e1f8a0ed6
Chris Woodrich <cwoodrich@gmail.com>
2014-02-28 14:52:07 -0800

    Adds seeding to db

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,12 @@
-BANDNAMES = [ "Joy Division", "New Order", "The Smiths", "The Sisters of Mercy", "Wye Oak", "Thao and the Get Down Stay Down",
-          "Siouxsie and the Banshees", "Interpol" ]
+require 'faker'
 
-BANDNAMES.each{ |band_name| Band.create(name: band_name) }
+
+10.times do |user|
+  User.create(email: Faker::Internet.email, password_hash: Faker::Lorem.word)
+  Survey.create(title: Faker::Company.name, user_id: rand(9)+1)
+  Question.create(text: Faker::Lorem.words, survey_id: rand(9)+1)
+  Response.create(user_id: rand(9)+1, answer_id: rand(9)+1)
+  Completesurvey.create(user_id: rand(9)+1, survey_id: rand(9)+1)
+  Answer.create(text: Faker::Lorem.sentence, question_id: rand(9)+1)
+end
+
