05d0c17996498f5b2832e9a0c78d8b2723e2738f
Quentin Devauchelle <quentin@devauchelle.eu>
2014-02-28 14:59:38 -0800

    Adds basic simple routes for the

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,49 @@
+get '/users/new' do
+  erb :'users/new'
+end
+
+post '/users/' do
+  # TODO: Refactor for alerts
+  @user = User.create(params[:user])
+  if @user.nil?
+    alert = "You actually need to fill the fields to create a user"
+    redirect "/users/new?alert=#{alert}"
+  elsif @user.invalid?
+    alert = "Not working"
+    redirect "/users/new?alert=#{alert}"
+  else
+    redirect "/users/#{@user.id}"
+  end
+end
+
+get '/users/:id/edit' do
+  @user = User.find(params[:id])
+  erb :'users/edit'
+end
+
+patch '/users/:id' do
+  # TODO: Refactor for alerts
+  @user = User.update(params[:id], params[:user])
+  if @user.invalid?
+    alert = "This entry is invalid for this user"
+  end
+  redirect "/users/edit#{"?alert="+alert if alert}"
+end
+
+get '/users/:id/delete' do
+  @user = User.find(params[:id])
+  erb :'users/delete'
+end
+
+delete '/users/:id/delete' do
+  User.find(params[:id].destroy)
+  if session[:id] == params[:id]
+    session.clear
+  end
+  redirect "/"
+end
+
+get '/users/:id' do
+  @user = User.find(params[:id])
+  erb :'users/show'
+end
