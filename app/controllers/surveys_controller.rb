79dd5b6c333da54bf35f9fd2049623faec1f4459
Henry Hsu <h_hsu817@yahoo.com>
2014-03-01 14:32:05 -0800

    wip. transfer to school comp for pair work

--- app/controllers/surveys_controller.rb
+++ app/controllers/surveys_controller.rb
@@ -1,26 +1,27 @@
 get '/surveys/new' do
-  redirect '/' unless logged_in?
   erb :'surveys/new'
 end
 
 post '/surveys/new' do
+  p "@@@@@@@"
+  puts params.inspect
   survey_params = params[:survey]
   @survey = Survey.create(title: survey_params[:survey])
-  Survery.questions.create(text: survey_params[:question1])
-  Survery.questions.answer.create(content: survey_params[:question1_answer1])
-  Survery.questions.answer.create(content: survey_params[:question1_answer2])
-  Survery.questions.answer.create(content: survey_params[:question1_answer3])
+  Survey.questions.create(text: survey_params[:question1])
+  Survey.questions.answer.create(content: survey_params[:question1_answer1])
+  Survey.questions.answer.create(content: survey_params[:question1_answer2])
+  Survey.questions.answer.create(content: survey_params[:question1_answer3])
 
 
-  Survery.questions.create(text: survey_params[:question2])
-  Survery.questions.answer.create(content: survey_params[:question2_answer1])
-  Survery.questions.answer.create(content: survey_params[:question2_answer2])
-  Survery.questions.answer.create(content: survey_params[:question2_answer3])
+  Survey.questions.create(text: survey_params[:question2])
+  Survey.questions.answer.create(content: survey_params[:question2_answer1])
+  Survey.questions.answer.create(content: survey_params[:question2_answer2])
+  Survey.questions.answer.create(content: survey_params[:question2_answer3])
 
-  Survery.questions.create(text: survey_params[:question3])
-  Survery.questions.answer.create(content: survey_params[:question3_answer1])
-  Survery.questions.answer.create(content: survey_params[:question3_answer2])
-  Survery.questions.answer.create(content: survey_params[:question3_answer3])
+  Survey.questions.create(text: survey_params[:question3])
+  Survey.questions.answer.create(content: survey_params[:question3_answer1])
+  Survey.questions.answer.create(content: survey_params[:question3_answer2])
+  Survey.questions.answer.create(content: survey_params[:question3_answer3])
 
   redirect "/user/#{current_user.id}"
 end
