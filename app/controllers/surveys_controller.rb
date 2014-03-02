57402fdd9e01cfd639eb6b3809e8a2ad745dc224
Apprentice <apprentice@dbc12.local>
2014-03-01 19:24:08 -0800

    Change typo in the route

--- app/controllers/surveys_controller.rb
+++ app/controllers/surveys_controller.rb
@@ -4,7 +4,7 @@ end
 
 post '/surveys/new' do
   survey_params = params[:survey]
-  @survey = Survey.create(title: survey_params[:survey])
+  @survey = Survey.create(title: survey_params[:survey], user_id: current_user.id)
   @question1 = @survey.questions.create(text: survey_params[:question1])
   @question1.answers.create(content: survey_params[:question1_answer1])
   @question1.answers.create(content: survey_params[:question1_answer2])
@@ -17,9 +17,9 @@ post '/surveys/new' do
   @question2.answers.create(content: survey_params[:question2_answer3])
 
   @question3 = @survey.questions.create(text: survey_params[:question3])
-  @question3.answer.create(content: survey_params[:question3_answer1])
-  @question3.answer.create(content: survey_params[:question3_answer2])
-  @question3.answer.create(content: survey_params[:question3_answer3])
+  @question3.answers.create(content: survey_params[:question3_answer1])
+  @question3.answers.create(content: survey_params[:question3_answer2])
+  @question3.answers.create(content: survey_params[:question3_answer3])
 
   redirect "/user/#{current_user.id}"
 end
