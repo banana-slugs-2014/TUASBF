c08aef1b802b29206ad0577e1e8557f6896afff7
Quentin Devauchelle <quentin@devauchelle.eu>
2014-03-02 13:06:56 -0800

    Correct the model tests to make them pass, according to the last model changes

--- spec/answer_spec.rb
+++ spec/answer_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe Answer do
-  it { should validate_presence_of :text }
-  it {should belong_to :question }
-  it {should have_many :responses }
+  it { should validate_presence_of :content }
+  it { should belong_to :question }
+  it { should have_many :responses }
 end
--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -6,6 +6,6 @@ describe User do
   it {should have_many :surveys}
   it {should have_many :responses}
   it {should validate_presence_of :email }
-  it {should validate_presence_of :password_hash}
+  it {should validate_presence_of :password}
 
 end
