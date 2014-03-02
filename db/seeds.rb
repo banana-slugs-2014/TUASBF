43284510b965664dffd9e50c7c2c73397531dff1
Apprentice <apprentice@dbc12.local>
2014-03-02 00:26:43 -0800

    Fix controller bug, fix Take Survey view

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,9 @@
 require 'faker'
 
 
-10.times do |user|
-  User.create(email: Faker::Internet.email, password: Faker::Lorem.word)
-  Survey.create(title: Faker::Company.name, user_id: rand(9)+1)
+10.times do |i|
+  User.create(email: "glen#{i}@glen.com", password: "asdf")
+  Survey.create(title: "Don't call me Glen, guys! No. #{i}", user_id: rand(9)+1)
   Question.create(text: Faker::Lorem.words, survey_id: rand(9)+1)
   Response.create(user_id: rand(9)+1, answer_id: rand(9)+1)
   Completesurvey.create(user_id: rand(9)+1, survey_id: rand(9)+1)
